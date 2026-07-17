f4edec64b81ed474f78bdc4f1be391f50c132864
Leo00709 <lucrenciopd@gmail.com>
2026-07-17 14:37:39 +0200

    Add CORS Middleware

---
+++ backend/app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', config('cors.allowed_origins', '*'))
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->header('Access-Control-Max-Age', '86400');
+    }
+}
